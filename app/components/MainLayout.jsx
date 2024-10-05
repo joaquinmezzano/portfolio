aba8633998197839ef2dd612aab81659cfaaf5f2
joaquinmezzano <mezzanojoaquin@gmail.com>
2024-10-05 18:01:46 -0300

    feature CustomSelectBox added

--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -7,7 +7,7 @@ import { AiOutlineHome } from "react-icons/ai";
 import { GrProjects } from "react-icons/gr";
 import { FaAngleRight, FaCheck, FaCheckDouble } from "react-icons/fa";
 import { FiPhoneCall } from "react-icons/fi";
-import { SiHelpscout } from "react-icons/si";
+import { SiHelpscout, SiSinglestore } from "react-icons/si";
 import { MenuContext } from "@/context/MenuContext";
 
 const MainLayout = ({ children }) => {
@@ -33,10 +33,18 @@ const MainLayout = ({ children }) => {
               <h3 className="flex-1">Projects</h3>
               <FaAngleRight />
             </li>
-            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-purple-600 rounded-xl p-2">
-              <FaCheck className="mr-2" />
-              <h3 className="flex-1">Singule</h3>
-              <FaAngleRight />
+            <li className="flex flex-col justify-start items-start hover:bg-blue-200 hover:text-purple-600 rounded-xl p-2">
+              <div className="w-full flex flex-row justify-start items-center">
+                <FaCheck className="mr-2" />
+                <h3 className="flex-1">Singular</h3>
+                <FaAngleRight />
+              </div>
+              <ul className="ml-8 mt-2">
+                <li className="flex justify-center items-center gap-3">
+                  <SiSinglestore />
+                  <Link href="/singular/selectbox">Select Box</Link>
+                </li>
+              </ul>
             </li>
             <li className="flex justify-start items-center hover:bg-blue-200 hover:text-red-600 rounded-xl p-2">
               <FaCheckDouble className="mr-2" />

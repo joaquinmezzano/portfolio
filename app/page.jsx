1728e595fad1f957e15faa81229dcf284c3722af
joaquinmezzano <mezzanojoaquin@gmail.com>
2024-10-05 10:38:44 -0300

    Clean up and configuration

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        earum molestias impedit nemo soluta architecto quasi, nam mollitia sunt
+        quae corporis exercitationem sequi, recusandae ullam facilis. Sed
+        perspiciatis ducimus quo!
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
